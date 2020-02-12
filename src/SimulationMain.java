152dae1e6285c9e5b2eb56fd970c6e6625fb64d7
Päivi Eversbusch <42863950+Finskdatamatiker@users.noreply.github.com>
2020-02-12 12:32:43 +0100

    Update SimulationMain.java

--- src/SimulationMain.java
+++ src/SimulationMain.java
@@ -4,11 +4,10 @@ public class SimulationMain {
     public static void main(String[] args) {
 
         Handler handler = new Handler();
-        int antalletAfIterationer = 30;
+        int antalletAfIterationer = 25;
 
         handler.makeFirstIteration();
 
-        //ret det her til ikke at have recursive kode
         for(int i = 0; i < antalletAfIterationer; i++) {
             handler.makeIteration();
         }
