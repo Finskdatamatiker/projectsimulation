4088aa97e42fc5a5b1ac0aeb1f15e8ef86b6cf6c
Päivi Eversbusch <paiv0018@stud.kea.dk>
2020-02-13 04:10:03 +0100

    Oprydning i kommentar

--- src/Handler.java
+++ src/Handler.java
@@ -145,7 +145,6 @@ public class Handler {
                     break;
             }
         }
-        //return iteration;
     }
 
 
--- src/Unit.java
+++ src/Unit.java
@@ -10,12 +10,10 @@ public class Unit {
      * value = bestemt mængde arbejde. dvs. procenter af det samlede projekt, fx. 0.0004 af det samlede projekt
      * round = hver state har et bestemt antal iterationer, så round fortæller, om unit
      * skal forblive i den samme state eller gå videre
-     * stateNr = state, som unit tilhører i denne iteration
      */
     private double value;
     private int round;
     private State state;
-    //private int stateNr;
 
     public Unit(){}
 
@@ -31,13 +29,6 @@ public class Unit {
     public void setRound(int round) {
         this.round = round;
     }
-    /*public int getStateNr() {
-        return stateNr;
-    }
-    public void setStateNr(int stateNr) {
-        this.stateNr = stateNr;
-    }*/
-
     public State getState() {
         return state;
     }
@@ -59,22 +50,7 @@ public class Unit {
         dfs.setDecimalSeparator('.');
         df.setDecimalFormatSymbols(dfs);
 
-        /*String stateName ="";
-        switch (stateNr){
-            case 0: stateName = State.STAR.toString();
-            break;
-            case 1: stateName = State.COMM.toString();
-            break;
-            case 2: stateName = State.PLAN.toString();
-            break;
-            case 3: stateName = State.MODE.toString();
-            break;
-            case 4: stateName = State.CONS.toString();
-            break;
-            case 5: stateName = State.DEPL.toString();
-            break;
-        }*/
-        return /*stateName +*/state +
+        return state +
                 " - value: " + df.format(value) +
                 ", round: " + round;
     }
